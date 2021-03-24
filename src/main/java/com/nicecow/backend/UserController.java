ab49849a06aef040dcefcbbcf929d75057456e0b
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-03-24 14:07:25 -0500

    added front end, doesn't start, somewhere there's a webpack devtool issue

--- src/main/java/com/nicecow/backend/UserController.java
+++ src/main/java/com/nicecow/backend/UserController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class HTTPEndpointsController {
+public class UserController {
     private UserRepository userRepository;
 
     @Autowired
-    public HTTPEndpointsController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
--- src/test/java/com/nicecow/backend/UserControllerTests.java
+++ src/test/java/com/nicecow/backend/UserControllerTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-public class HTTPEndpointsControllerTests {
+public class UserControllerTests {
 
     @Autowired
     private UserRepository userRepository;
