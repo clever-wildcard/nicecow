0c1a474479155615c443b90c991b46acefbca894
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-03-31 13:45:01 -0500

    putting a pin in this

--- src/main/java/com/nicecow/backend/UserController.java
+++ src/main/java/com/nicecow/backend/UserController.java
@@ -1,10 +1,13 @@
 package com.nicecow.backend;
 
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,10 +23,19 @@ public class UserController {
     public User saveUser(@RequestBody User user) {
         return this.userRepository.save(user);
     }
-
+    
     @GetMapping("/user/all")
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(this.userRepository.findAll());
+    public ResponseEntity<HashMap> getUsers() {
+        List<JSONObject> jsonObjects = new ArrayList<>();
+        for (User user: userRepository.findAll()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", user.getName());
+            jsonObject.put("id", user.getId());
+            jsonObjects.add(jsonObject);
+        }
+        HashMap<String, List<JSONObject>> jsonMap = new HashMap<>();
+        jsonMap.put("users", jsonObjects);
+        return ResponseEntity.ok(jsonMap);
     }
 
     @GetMapping("/user/{id}")
@@ -58,11 +70,4 @@ public class UserController {
         this.userRepository.delete(user);
         return ResponseEntity.ok().build();
     }
-
-//    @RequestMapping("/api/login")
-//    User login(@RequestBody User user) {
-//        return user;
-//    }
-
-
 }
